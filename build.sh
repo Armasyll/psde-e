223679d0cf3fb20ae16efa863711a5cc6d83c15f
armasyll <armasyll@gmail.com>
2021-03-13 19:00:26 -0500

    Getting the absolute path is a bitch

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+SCRIPTPATH="$( cd -- "$(dirname "BASH_SOURCE[0])")" > /dev/null 2>&1 ; pwd -P )"
 electron-packager . PSDEe --platfor=linux --arch-x64 --electron-version=11.2.0
+rm -R PSDEe-linux-x64/resources/app/
+ln -s $SCRIPTPATH PSDEe-linux-x64/resources/app
