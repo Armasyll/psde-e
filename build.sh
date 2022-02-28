8bb4f195e69af84c1559d357b280693f7766bc55
armasyll <armasyll@gmail.com>
2022-02-28 14:54:19 -0500

    Check what you're deleting :V

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "BASH_SOURCE[0])")" > /dev/null 2>&1 ; pwd -P )"
+APPPATH="PSDEe-linux-x64/resources/app"
 npx electron-packager . PSDEe --platfor=linux --arch-x64 --electron-version=11.2.0
-rm -R PSDEe-linux-x64/resources/app/
-ln -s $SCRIPTPATH PSDEe-linux-x64/resources/app
+if [ -L "$APPATH" ] && [ -d "$APPATH" ]; then
+    rm PSDEe-linux-x64/resources/app
+elif [ -d "$APPATH/" ]; then
+    rm -R PSDEe-linux-x64/resources/app
+elif [ -f "$APPATH" ]; then
+    rm PSDEe-linux-x64/resources/app
+fi
+ln -s $SCRIPTPATH $APPDATH
