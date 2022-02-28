d66d376582e4400eec926c59c37fa1fdf21a7b90
armasyll <armasyll@gmail.com>
2022-02-28 14:56:40 -0500

    Fire whoever wrote that last push :v incorrect variable names

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "BASH_SOURCE[0])")" > /dev/null 2>&1 ; pwd -P )"
 APPPATH="PSDEe-linux-x64/resources/app"
-npx electron-packager . PSDEe --platfor=linux --arch-x64 --electron-version=11.2.0
-if [ -L "$APPATH" ] && [ -d "$APPATH" ]; then
+#npx electron-packager . PSDEe --platfor=linux --arch-x64 --electron-version=11.2.0
+if [ -L "$APPPATH" ] && [ -d "$APPPATH" ]; then
     rm PSDEe-linux-x64/resources/app
-elif [ -d "$APPATH/" ]; then
+elif [ -d "$APPPATH/" ]; then
     rm -R PSDEe-linux-x64/resources/app
-elif [ -f "$APPATH" ]; then
+elif [ -f "$APPPATH" ]; then
     rm PSDEe-linux-x64/resources/app
 fi
-ln -s $SCRIPTPATH $APPDATH
+ln -s $SCRIPTPATH $APPPATH
